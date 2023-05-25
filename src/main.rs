0a86e8a11ac3bbe788a8fffcdbdc642944a2cb12
Dwogo <philliptdavies@gmail.com>
2023-05-25 17:13:18 +1200

    GTK added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("dev.dwogo.Gswww")
+        .build();
+
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Gswww")
+        .build();
+
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Open File Dialog")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|_| gtk::FileDialog);
+    window.set_child(Some(&button));
+
+    window.present();
 }
